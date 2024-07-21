291f46dbca2b2bf5aee153dd4c3dd10fa5067cfd
alasdair <alasdair.c.mott@gmail.com>
2024-07-22 09:13:19 +1000

    island

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
-use bevy::asset::AssetMetaCheck;
+use bevy::pbr::NotShadowReceiver;
 use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, pbr::CascadeShadowConfigBuilder};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use std::f32::consts::PI;
+
+static BG_COLOR: Color = Color::srgb(0.2, 0.76, 1.0);
 
 fn main() {
     App::new()
@@ -16,14 +21,104 @@ fn main() {
             meta_check: AssetMetaCheck::Never,
             ..default()
         }))
-        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(BG_COLOR))
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Startup, (setup, spawn_gltf, spawn_water))
+        .add_systems(Update, simulate_tides)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
+            ..default()
+        },
+        PanOrbitCamera::default(),
+        FogSettings {
+            color: BG_COLOR,
+            falloff: FogFalloff::Linear {
+                start: 5.0,
+                end: 20.0,
+            },
+            ..default()
+        },
+    ));
+
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: light_consts::lux::FULL_DAYLIGHT,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 20.0,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
+}
+
+fn spawn_gltf(mut commands: Commands, ass: Res<AssetServer>) {
+    // note that we have to include the `Scene0` label
+    let my_gltf = ass.load("island.glb#Scene0");
+
+    // to position our 3d model, simply use the Transform
+    // in the SceneBundle
+    commands.spawn(SceneBundle {
+        scene: my_gltf,
+        transform: Transform::from_xyz(2.0, 0.0, -5.0),
         ..Default::default()
     });
+}
+
+#[derive(Component)]
+pub struct Water;
+
+fn spawn_water(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let water_material = materials.add(StandardMaterial {
+        base_color: Color::srgb(0.0, 0.5, 0.7),
+        reflectance: 0.3,
+        ..Default::default()
+    });
+
+    let mesh_size = 50.0;
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(
+                Plane3d::default()
+                    .mesh()
+                    .size(mesh_size, mesh_size)
+                    .subdivisions(1),
+            ),
+            material: water_material,
+            ..default()
+        },
+        NotShadowReceiver,
+        Water,
+    ));
+}
+
+fn simulate_tides(time: Res<Time>, mut query: Query<&mut Transform, With<Water>>) {
+    let wave_speed = 0.2;
+    let wave_height = 0.3;
+
+    for mut transform in query.iter_mut() {
+        transform.translation.y = (time.elapsed_seconds() as f32 * wave_speed).sin() * wave_height;
+    }
 }
